4f11d2b33572ede5140bee3132a0d5382ee7470b
zhangyx <yuxinzh1226@qq.com>
2023-12-15 00:15:49 +0800

    init commit

---
+++ gulimall-member/src/test/java/com/zyx/gulimall/member/GulimallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zyx.gulimall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gulimall-product/src/main/java/com/zyx/gulimall/product/GulimallProductApplication.java
@@ -0,0 +1,13 @@
+package com.zyx.gulimall.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallProductApplication.class, args);
+    }
+
+}
