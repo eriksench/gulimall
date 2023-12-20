478d73f32e83c3211fc9155b5f07ac9c52445450
zhangyuxin <yuxinzh1226@qq.com>
2023-12-20 15:22:26 +0800

    fix:gulimall父工程引用了product模块的代码，导致无法启动

--- gulimall-product/src/test/java/com/zyx/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/zyx/gulimall/product/GulimallProductApplicationTests.java
@@ -1,13 +1,22 @@
 package com.zyx.gulimall.product;
 
+import com.zyx.gulimall.product.entity.BrandEntity;
+import com.zyx.gulimall.product.service.BrandService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GulimallProductApplicationTests {
 
+    @Autowired
+    BrandService brandService;
+
     @Test
     void contextLoads() {
+        BrandEntity brandEntity = new BrandEntity();
+        brandEntity.setName("huawei");
+        brandService.save(brandEntity);
     }
 
 }
